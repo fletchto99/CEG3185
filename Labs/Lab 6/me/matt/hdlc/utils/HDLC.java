83d986b1299a4d9effd6278418c612f41926f516
Nikaya <nsnel078@uottawa.ca>
2017-03-21 15:04:53 -0400

    Update HDLC.java

--- Labs/Lab 6/me/matt/hdlc/utils/HDLC.java
+++ Labs/Lab 6/me/matt/hdlc/utils/HDLC.java
@@ -6,21 +6,10 @@ import java.util.BitSet;
  * Created by mattlanglois on 2017-03-15.
  */
 public class HDLC {
-	public static final BitSet FLAG = createFromString("01111110");
-    public static final BitSet FCS = createFromString("0000000000000000");
-    public static final BitSet CONTROL_SNRM = createFromString("11001001");
-    public static final BitSet CONTROL_UA = createFromString("11001110");
-    public static final BitSet CONTROL_INFO = createFromString("00010000");
-    public static final BitSet CONTROL_RR = createFromString("10001000");
-
-
-    private static BitSet createFromString(final String data) {
-        BitSet bitset = new BitSet(data.length());
-        for (int i = 0; i < data.length(); i++) {
-            if (data.charAt(i) == '1') {
-                bitset.set(i);
-            }
-        }
-        return bitset;
-    }
+	public static final String FLAG = "01111110";
+        public static final String FCS = "0000000000000000";
+        public static final String CONTROL_SNRM = "11001001";
+        public static final String CONTROL_UA = "11001110";
+        public static final String CONTROL_INFO = "00010000";
+        public static final String CONTROL_RR = "10001000";
 }
