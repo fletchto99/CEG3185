d442c2d982759ea697d05897c6f93a302f2964d4
Nikaya <nsnel078@uottawa.ca>
2017-03-21 13:11:50 -0400

    Update HDLC.java

--- Labs/Lab 6/me/matt/hdlc/utils/HDLC.java
+++ Labs/Lab 6/me/matt/hdlc/utils/HDLC.java
@@ -4,11 +4,11 @@ package me.matt.hdlc.utils;
  * Created by mattlanglois on 2017-03-15.
  */
 public class HDLC {
-  public static final BitSet FLAG = "01111110";
-	public static final BitSet FCS = "0000000000000000";
-	public static final BitSet CONTROL_SNRM = "11001001";
-	public static final BitSet CONTROL_UA = "11001110";
-	public static final BitSet CONTROL_INFO = "00010000";
-	public static final BitSet CONTROL_RR = "10001000";
-
+	public static final BitSet FLAG = "01111110";
+	public static final BitSet FLAG = 01111110;
+        public static final BitSet FCS = 0000000000000000;
+        public static final BitSet CONTROL_SNRM = 11001001;
+        public static final BitSet CONTROL_UA = 11001110;
+        public static final BitSet CONTROL_INFO = 00010000;
+        public static final BitSet CONTROL_RR = 10001000;
 }
