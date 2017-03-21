8edd763764fcf55f19ec379261869f910104e7d1
Nikaya <nsnel078@uottawa.ca>
2017-03-20 21:54:42 -0400

    Update Constants.java

--- Labs/Lab 6/me/matt/hdlc/utils/Constants.java
+++ Labs/Lab 6/me/matt/hdlc/utils/Constants.java
@@ -1,7 +1,11 @@
 package me.matt.hdlc.utils;
 
+import java.util.BitSet;
+
 public class Constants {
 
     public static final int DEFAULT_PORT = 8085;
+    public static final BitSet SNRM = 11001001;
+    public static final BitSet Flag = 01111110;
 
 }
