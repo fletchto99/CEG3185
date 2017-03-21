1831283b2648171a7d75f55df2fcca0394f6fd36
Nikaya <nsnel078@uottawa.ca>
2017-03-21 16:16:22 -0400

    Update HDLC.java

--- Labs/Lab 6/me/matt/hdlc/utils/HDLC.java
+++ Labs/Lab 6/me/matt/hdlc/utils/HDLC.java
@@ -9,6 +9,7 @@ public class HDLC {
     public static final String CONTROL_INFO = "00010000";
     public static final String CONTROL_RRP = "10001000";
     public static final String CONTROL_RRF= "10011000";
+    public static final String ADDRESS_A="00000010";
     public static final String ADDRESS_B="00000000";
     public static final String ADDRESS_C="00000001";
 }
