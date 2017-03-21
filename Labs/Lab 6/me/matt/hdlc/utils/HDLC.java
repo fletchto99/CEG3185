02667c1ba3c40e0c5c6fb4d13043588e31d71836
Nikaya <nsnel078@uottawa.ca>
2017-03-21 16:01:51 -0400

    Update HDLC.java

--- Labs/Lab 6/me/matt/hdlc/utils/HDLC.java
+++ Labs/Lab 6/me/matt/hdlc/utils/HDLC.java
@@ -2,12 +2,13 @@ package me.matt.hdlc.utils;
 
 public class HDLC {
 
-    public static final String FLAG         = "01111110";
-    public static final String FCS          = "0000000000000000";
+    public static final String FLAG = "01111110";
+    public static final String FCS = "0000000000000000";
     public static final String CONTROL_SNRM = "11001001";
-    public static final String CONTROL_UA   = "11001110";
+    public static final String CONTROL_UA = "11001110";
     public static final String CONTROL_INFO = "00010000";
     public static final String CONTROL_RRP = "10001000";
     public static final String CONTROL_RRF= "10011000";
-
+    public static final String ADDRESS_B="00000000";
+    public static final String ADDRESS_C="00000001";
 }
