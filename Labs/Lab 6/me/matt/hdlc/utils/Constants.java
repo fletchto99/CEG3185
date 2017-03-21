fe29d09314fe0ebf05574dcf385a5a703c063549
Nikaya <nsnel078@uottawa.ca>
2017-03-21 10:09:05 -0400

    Update Constants.java

--- Labs/Lab 6/me/matt/hdlc/utils/Constants.java
+++ Labs/Lab 6/me/matt/hdlc/utils/Constants.java
@@ -5,7 +5,11 @@ import java.util.BitSet;
 public class Constants {
 
     public static final int DEFAULT_PORT = 8085;
-    public static final BitSet SNRM = 11001001;
-    public static final BitSet FLAG = 01111110;
+    public static final BitSet FLAG = "01111110";
+	public static final BitSet FCS = "0000000000000000";
+	public static final BitSet CONTROL_SNRM = "11001001";
+	public static final BitSet CONTROL_UA = "11001110";
+	public static final BitSet CONTROL_INFO = "00010000";
+	public static final BitSet CONTROL_ACK = "10001000";
 
 }
