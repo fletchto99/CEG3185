05ea4420d4667f6e4c7d33e3e0927012d176fcc6
Nikaya <nsnel078@uottawa.ca>
2017-03-21 21:40:41 -0400

    Update Application.java

--- Labs/Lab 6/me/matt/hdlc/Application.java
+++ Labs/Lab 6/me/matt/hdlc/Application.java
@@ -5,7 +5,8 @@ public class Application {
     public static void main(String... args) {
         System.out.println("Starting app");
         new Thread(new Server()).start();
-        new Thread(new Client()).start();
+        new Thread(new ClientB()).start();
+        new Thread(new ClientC()).start();
     }
 
 }
